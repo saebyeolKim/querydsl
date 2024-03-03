319a2e6024943a50c7f165f3dfaf832a3c8c0f5b
saebyeolKim <toquf2611@naver.com>
2024-03-03 17:45:08 +0900

    feat: 예제 도메인 모델과 동작확인

---
+++ src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,42 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"}) //team 은 들어가면 안된다. 무한루프를 타기 때문. 본인 소유 필드만 하기
+public class Member {
+
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this(username, 0);
+    }
+
+    public Member(String username, int age) {
+        this(username, age, null);
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    private void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
