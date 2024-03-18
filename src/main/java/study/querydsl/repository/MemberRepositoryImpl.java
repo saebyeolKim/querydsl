cb1264206a215e2eb1c5b10079d7615e1c7680c4
saebyeolKim <toquf2611@naver.com>
2024-03-18 21:30:26 +0900

    feat: 스프링 데이터 페이징 활용2 - CountQuery 최적화

--- src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -2,11 +2,13 @@ package study.querydsl.repository;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
@@ -97,7 +99,7 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                 .limit(pageable.getPageSize())
                 .fetch();
         //카운트할 때 조인이 필요하지 않을 수도 있다. (카운트 쿼리 최적화 시 이렇게 사용)
-        long total = queryFactory
+        JPAQuery<Member> countQuery = queryFactory
                 .select(member)
                 .from(member)
                 .leftJoin(member.team, team)
@@ -106,10 +108,12 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                         teamNameEa(condition.getTeamName()),
                         ageGoe(condition.getAgeGoe()),
                         ageLoe(condition.getAgeLoe())
-                )
-                .fetchCount();
+                );
 
-        return new PageImpl<>(content, pageable, total);
+        //마지막 페이지거나 컨텐츠가 없으면, 페이지에 10개를 불러야되는데 3개밖에 없을 때 카운트쿼리를 날리지 않음
+        return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
+//        return PageableExecutionUtils.getPage(content, pageable, () -> countQuery.fetchCount());
+//        return new PageImpl<>(content, pageable, total);
     }
 
     private BooleanExpression usernameEq(String username) {
